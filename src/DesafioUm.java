7d040ab4af58f62a1f7b18ad4ba303ec1373c734
lipesc <felipedrhouse@hotmail.com>
2024-06-10 14:02:38 -0300

    desadios logica DIO

---
+++ src/DesafioUm.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DesafioUm {
+    public static void main(String[] args) throws Exception {
+        // read the input
+        Scanner scanner = new Scanner(System.in);
+        float valorSalario = scanner.nextFloat();
+        float valorBeneficios = scanner.nextFloat();
+
+        // close the scanner
+        scanner.close();
+
+        // calculate the salary
+        float imposto = 0;
+        if (valorSalario >= 0 && valorSalario <= 1100)
+            // if the salary is between 0 and 1100, the tax is 0.05
+            imposto = 0.05f * valorSalario;
+
+        // print the result
+        float saida = valorSalario - imposto + valorBeneficios;
+        System.out.printf("%.2f", saida);
+    }
+
+}
